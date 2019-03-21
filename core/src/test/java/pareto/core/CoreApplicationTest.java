d717c97661597fcad4042be895183223bf8f69b6
sergey <bugakov.sa.88@gmail.com>
2019-03-21 11:20:22 +0300

    PlayProcess

--- core/src/test/java/pareto/core/CoreApplicationTest.java
+++ core/src/test/java/pareto/core/CoreApplicationTest.java
@@ -189,16 +189,26 @@ public class CoreApplicationTest {
 
     private List<PlayPnlDto> getExpectedPlayPnls() {
         return List.of(
+                //positions 0 orders 0
                 new PlayPnlDto(parse("2019-02-10T15:00:00"), 100000, 100000, 100000, 100000),
+                //positions 0 orders 1
                 new PlayPnlDto(parse("2019-02-10T15:01:00"), 100000, 100000, 100000, 100000),
+                //positions 1 orders 0
                 new PlayPnlDto(parse("2019-02-10T15:02:00"), 100000, 100017, 100000, 100028),
+                //positions 1 orders 0
                 new PlayPnlDto(parse("2019-02-10T15:03:00"), 100017, 100067, 100016, 100108),
-                new PlayPnlDto(parse("2019-02-10T15:04:00"), 100000, 100000, 100000, 100000),
-                new PlayPnlDto(parse("2019-02-10T15:05:00"), 100000, 100000, 100000, 100000),
-                new PlayPnlDto(parse("2019-02-10T15:06:00"), 100000, 100000, 100000, 100000),
-                new PlayPnlDto(parse("2019-02-10T15:07:00"), 100000, 100000, 100000, 100000),
-                new PlayPnlDto(parse("2019-02-10T15:08:00"), 100000, 100000, 100000, 100000),
-                new PlayPnlDto(parse("2019-02-10T15:09:00"), 100000, 100000, 100000, 100000)
+                //positions 1 orders 1
+                new PlayPnlDto(parse("2019-02-10T15:04:00"), 100067, 100136, 100016, 100179),
+                //positions 0 orders 0
+                new PlayPnlDto(parse("2019-02-10T15:05:00"), 100136, 100136, 100136, 100136),
+                //positions 0 orders 0
+                new PlayPnlDto(parse("2019-02-10T15:06:00"), 100136, 100136, 100136, 100136),
+                //positions 0 orders 1
+                new PlayPnlDto(parse("2019-02-10T15:07:00"), 100136, 100136, 100136, 100136),
+                //positions 1 orders 0
+                new PlayPnlDto(parse("2019-02-10T15:08:00"), 100136, 100138, 100127, 100139),
+                //positions 1 orders 0
+                new PlayPnlDto(parse("2019-02-10T15:09:00"), 100138, 100188, 100137, 100229)
         );
     }
 
